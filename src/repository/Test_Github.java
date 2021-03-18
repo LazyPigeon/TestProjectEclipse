217ec878e670c9e94aa1e4afa64d95600b1fab5d
Reins Janovskis <reinis.janovskis@hotmail.com>
2021-03-18 13:06:18 +0000

    Testing Github.
    Hello World variation.

---
+++ src/repository/Test_Github.java
@@ -0,0 +1,10 @@
+package repository;
+
+public class Test_Github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Hello, Github - my old friend!\n");
+	}
+
+}
